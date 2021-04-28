226904dd320a783ca83f59c450de0d60f1eab2a0
DawnPeanut <1540680885@qq.com>
2021-04-28 22:22:10 +0800

    first commit

---
+++ src/main/java/com/fzh/demo/gitdemo/GitdemoApplication.java
@@ -0,0 +1,17 @@
+package com.fzh.demo.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+
+}
